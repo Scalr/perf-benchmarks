e215a2d5c70f6f718ee38169da75443afc27f066
Roman Koshel <roman@scalr.com>
2013-05-06 16:19:20 +0300

    developing

--- region-io/nc_start.sh
+++ region-io/nc_start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-USER=$1
-DEST_IP=$2
-SSH_KEY=$HOME/.ssh/id_rsa
-NETCAT_PORT=666
-DEST=/tmp/io_benchmarks
+NETCAT_PORT=$1
+DEST=$2
 
 echo 'run netcat on destination server'
-#ssh -i $SSH_KEY $USER@$DEST_IP 'sudo nc -l 666 > /dev/null'
-sudo nc -l 666 > $DEST
+sudo nc -l $NETCAT_PORT > $DEST
