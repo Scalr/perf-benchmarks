8ade11d3eb7fb93545519713186e335fffc49216
Ubuntu <ubuntu@ec2-50-18-21-65.us-west-1.compute.amazonaws.com>
2013-05-06 13:38:59 +0000

    developing

--- region-io/cross_region_io.sh
+++ region-io/cross_region_io.sh
@@ -3,13 +3,13 @@
 USER=$1
 DEST_IP=$2
 FILE_SIZE=$3
+NETCAT_PORT=$4
 SSH_KEY=$HOME/.ssh/id_rsa
 SOURCE=/tmp/io_benchmarks
-DEST=/tmp/io_benchmarks
-NETCAT_PORT=666
+DEST=$5
 LOG=log/region-io
 
-if [ -f log ]; then
+if [ ! -d log ]; then
     mkdir log
 fi
 
@@ -24,6 +24,4 @@ echo "NetCut" >> $LOG
 
 echo "Single thread scp" >> $LOG
 { /usr/bin/time -f "%E Elapsed\n%U User\n%S System\n%M Memory\n%P Percenage of the CPU\n" /usr/bin/scp -i $SSH_KEY $SOURCE $USER@$DEST_IP:$DEST; } 2>>$LOG
-
-echo -e "\n" >> $LOG
 
