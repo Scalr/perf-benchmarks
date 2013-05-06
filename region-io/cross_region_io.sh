a7f976dda77034bc3d275e3f8508c7f93a6904e7
Roman Koshel <roman@scalr.com>
2013-05-06 16:05:56 +0300

    log

--- region-io/cross_region_io.sh
+++ region-io/cross_region_io.sh
@@ -3,11 +3,15 @@
 USER=$1
 DEST_IP=$2
 FILE_SIZE=$3
-LOG=$4
 SSH_KEY=$HOME/.ssh/id_rsa
 SOURCE=/tmp/io_benchmarks.file
 DEST=/tmp/io_benchmarks.file
 NETCAT_PORT=666
+LOG=log/region-io
+
+if [ -f log ]; then
+    mkdir log
+fi
 
 truncate -s $FILE_SIZE $SOURCE
 
@@ -21,4 +25,5 @@ echo "NetCut" >> $LOG
 echo "Single thread scp" >> $LOG
 { /usr/bin/time -f "%E Elapsed\n%U User\n%S System\n%M Memory\n%P Percenage of the CPU\n" /usr/bin/scp -i $SSH_KEY $SOURCE $USER@$DEST_IP:$DEST; } 2>>$LOG
 
-echo -e "\n"
+echo -e "\n" >> $LOG
+
