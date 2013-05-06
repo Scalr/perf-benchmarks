7b1da576e2804ed1905bf2ca6de7c6606c216dca
Vagrant <vagrant@vagrant-ubuntu.vagrantup.com>
2013-05-06 17:32:03 -0300

    Rename; Log date

--- region-io/cross_region_io_2.sh
+++ region-io/cross_region_io_2.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 
-NETCAT_PORT=$1
-DEST=$2
+SSH_KEY=$1
+USER=$2
+DEST_IP=$3
+FILE_SIZE=$4
+NETCAT_PORT=$5
+CLOUD=$6
+SOURCE=/tmp/io_benchmarks
+DEST=/dev/null
+LOG=results/region-io-`date +"%Y-%m-%d-%H:%M:%S"`
 
-echo 'run netcat on destination server'
-sudo nc -l $NETCAT_PORT > $DEST
+if [ ! -d results ]; then
+    mkdir results
+fi
+
+truncate -s $FILE_SIZE $SOURCE
+
+echo "# cross-region io benchmark" >> $LOG
+echo `/bin/date` >> $LOG
+
+if [ "$CLOUD" == "ec2" ]; then
+    echo `/usr/bin/ec2metadata | grep 'availability-zone'` >> $LOG
+    echo `/usr/bin/ec2metadata | grep 'machine'` >> $LOG
+fi
+
+if [ "$CLOUD" == "gce" ]; then
+    echo `/usr/bin/gcutil getinstance $HOSTNAME | grep 'zone'` >> $LOG
+    echo `/usr/bin/gcutil getinstance $HOSTNAME | grep 'zone'` >> $LOG
+fi
+
+echo -e "File size" $FILE_SIZE "\n" >> $LOG
+
+echo "nc" >> $LOG
+{ /usr/bin/time -f "%E Elapsed\n%U User\n%S System\n%M Memory\n%P Percenage of the CPU\n" /bin/cat $SOURCE | /bin/nc $DEST_IP $NETCAT_PORT ; } 2>>$LOG
+
+echo "scp" >> $LOG
+{ /usr/bin/time -f "%E Elapsed\n%U User\n%S System\n%M Memory\n%P Percenage of the CPU\n" /usr/bin/scp -i $SSH_KEY $SOURCE $USER@$DEST_IP:$DEST; } 2>>$LOG
 
 # FIXME with chef
 sudo apt-get install -y iperf
 
-echo 'run iperf server on destination server'
-/usr/bin/iperf -s -p 12345
+echo "iperf" >> $LOG
+/usr/bin/iperf -c $DEST_IP -p 12345 -t 60 | grep 'Gbits/sec' >> $LOG
