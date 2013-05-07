ce91d32f1cfedc907193f18a4fddfa96ea3ef025
Roman Koshel <roman@scalr.com>
2013-05-07 14:38:14 +0300

    fix

--- region-io/cross_region_io_2.sh
+++ region-io/cross_region_io_2.sh
@@ -25,7 +25,7 @@ if [ "$CLOUD" == "ec2" ]; then
     echo 'From:' >> $LOG
     echo `ec2metadata | grep 'availability-zone'` >> $LOG
     echo 'To:' >> $LOG
-    ssh -i $HOME/.ssh/id_rsa -l root $DEST_IP 'ec2metadata | grep availability-zone' >> $LOG
+    ssh -i $HOME/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $USER $DEST_IP 'ec2metadata | grep availability-zone' >> $LOG
     echo 'Type:' >> $LOG
     echo `ec2metadata | grep 'instance-type'` >> $LOG
 fi
@@ -34,7 +34,7 @@ if [ "$CLOUD" == "gce" ]; then
     echo 'From:' >> $LOG
     echo `gcutil getinstance $HOSTNAME | grep 'zone'` >> $LOG
     echo 'To:' >> $LOG
-    ssh -i $HOME/.ssh/id_rsa -l root $DEST_IP 'gcutil getinstance $HOSTNAME | grep zone' >> $LOG
+    ssh -i $HOME/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $USER $DEST_IP 'gcutil getinstance $HOSTNAME | grep zone' >> $LOG
     echo 'Type:' >> $LOG
     echo `gcutil getinstance $HOSTNAME | grep 'machine'` >> $LOG
 fi
@@ -46,11 +46,11 @@ echo "netcat:" >> $LOG
 { /usr/bin/time -f "%E Elapsed\n%U User\n%S System\n%M Memory\n%P Percentage of the CPU" cat $SOURCE | nc $DEST_IP $NETCAT_PORT -q 0; } 2>>$LOG
 
 echo "scp:" >> $LOG
-{ /usr/bin/time -f "%E Elapsed\n%U User\n%S System\n%M Memory\n%P Percentage of the CPU" scp -i $SSH_KEY -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking $SOURCE $USER@$DEST_IP:$DEST; } 2>>$LOG
+{ /usr/bin/time -f "%E Elapsed\n%U User\n%S System\n%M Memory\n%P Percentage of the CPU" scp -i $SSH_KEY -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SOURCE $USER@$DEST_IP:$DEST; } 2>>$LOG
 
 echo "iperf:" >> $LOG
 iperf -c $DEST_IP -p 12345 -t 60 | grep '/sec' >> $LOG
 
 
-ssh -i $SSH_KEY -l $USER $DEST_IP 'sudo killall -9 nc'
-ssh -i $SSH_KEY -l $USER $DEST_IP 'sudo killall -9 iperf'
+ssh -i $SSH_KEY -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $USER $DEST_IP 'sudo killall -9 nc'
+ssh -i $SSH_KEY -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $USER $DEST_IP 'sudo killall -9 iperf'
