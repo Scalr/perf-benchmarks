054a052985171742fdfdf007572cf34781cd8d7f
Roma Koshel <roma.koshel@gmail.com>
2013-05-10 02:32:46 +0300

    network-io python

--- network-io/network_io.py
+++ network-io/network_io.py
@@ -15,41 +15,34 @@ import multiprocessing as mp
 ssh_key = '%s/.ssh/id_rsa' % os.environ['HOME']
 
 
-def network_io_test(
+def ec2_network_io_test(
         inst_type='m1.small',
         ami1='ami-883714cd', ami2='ami-4ac9437a',
         region1='us-west-1', region2='us-west-2',
         filesize='5G'):
 
-    f = open('%s.csv' % inst_type, 'w+')
-    f.write('date;time;netcat;scp;iperf;iperfd;iperf4;iper8;iperf16\n')
-    f.flush()
-    f.close()
-
     conn1 = boto.ec2.connect_to_region(region1)
     conn2 = boto.ec2.connect_to_region(region2)
 
-    print 'run first instance'
     reservation1 = conn1.run_instances(
             ami1,
-            key_name='perf-bench-us-west-1',
+            key_name='perf-bench-%s' % region1,
             instance_type=inst_type,
-            security_groups=['perf-bench-us-west-1'])
+            security_groups=['perf-bench-%s' % region1])
 
     inst1=reservation1.instances[0]
 
-    print inst1
+    print inst_type, inst1
 
-    print 'run second instance'
     reservation2 = conn2.run_instances(
             ami2,
-            key_name='perf-bench-us-west-2',
+            key_name='perf-bench-%s' % region2,
             instance_type=inst_type,
-            security_groups=['perf-bench-us-west-2'])
+            security_groups=['perf-bench-%s' % region2])
 
     inst2=reservation2.instances[0]
 
-    print inst2
+    print inst_type, inst2
 
     try:
 
@@ -68,8 +61,8 @@ def network_io_test(
             except:
                 time.sleep(1)
 
-        print inst1.ip_address
-        print inst2.ip_address
+        print inst_type, inst1.ip_address
+        print inst_type, inst2.ip_address
 
         ssh_cli1 = paramiko.SSHClient()
         ssh_cli1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -91,46 +84,46 @@ def network_io_test(
         scp_cli2.put('%s/.ssh/id_rsa.pub' % os.environ['HOME'], '/tmp/id_rsa.pub')
 
         stdin, stdout, stderr = ssh_cli1.exec_command('cat /tmp/id_rsa.pub >> $HOME/.ssh/authorized_keys')
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli1.exec_command('mv /tmp/id_rsa $HOME/.ssh/')
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli1.exec_command('mv /tmp/id_rsa.pub $HOME/.ssh/')
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli1.exec_command('chmod 400 $HOME/.ssh/id_rsa*')
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli2.exec_command('cat /tmp/id_rsa.pub >> $HOME/.ssh/authorized_keys')
-        print stderr.read()
+        print inst_type, stderr.read()
         
         stdin, stdout, stderr = ssh_cli2.exec_command('mv /tmp/id_rsa $HOME/.ssh/')
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli2.exec_command('mv /tmp/id_rsa.pub $HOME/.ssh/')
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli2.exec_command('chmod 400 $HOME/.ssh/id_rsa*')
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli1.exec_command('ssh-keyscan github.com >> $HOME/.ssh/known_hosts')
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli1.exec_command('ssh-keyscan %s >> $HOME/.ssh/known_hosts' % inst2.ip_address)
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli1.exec_command('ssh-keyscan %s >> $HOME/.ssh/authorized_keys' % inst2.ip_address)
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli2.exec_command('ssh-keyscan github.com >> $HOME/.ssh/known_hosts')
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli2.exec_command('ssh-keyscan %s >> $HOME/.ssh/known_hosts' % inst1.ip_address)
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli2.exec_command('ssh-keyscan %s >> $HOME/.ssh/authorized_keys' % inst1.ip_address)
-        print stderr.read()
+        print inst_type, stderr.read()
 
         stdin, stdout, stderr = ssh_cli1.exec_command('export DEBIAN_FRONTEND=noninteractive;sudo apt-get install -y iperf fio')
         stdout.channel.recv_exit_status()
@@ -144,34 +137,37 @@ def network_io_test(
         ssh_cli2.close()
 
         today = datetime.datetime.today()
-        f = open('%s.csv' % inst_type, 'a+')
 
         netcat_start_time = time.time()
-        netcat_time = netcat_test(inst1.ip_address, inst2.ip_address, '10M')
-        print 'netcat;%i:%s;' % (netcat_start_time, netcat_time)
+        netcat_time = netcat_test(inst1.ip_address, inst2.ip_address, filesize)
+        print inst_type, 'netcat;%s;' % netcat_time
 
         scp_start_time = time.time()
-        scp_time = scp_test(inst1.ip_address, inst2.ip_address, '10M')
-        print 'scp;%i:%s;' % (scp_start_time, scp_time)
+        scp_time = scp_test(inst1.ip_address, inst2.ip_address, filesize)
+        print inst_type, 'scp;%s;' % scp_time
 
-        speed = iperf_test(inst1.ip_address, inst2.ip_address)
+        speed1, speed4, speed8, speed12, speed16 = iperf_test(inst1.ip_address, inst2.ip_address)
 
-        f.write('%s;%s;%s;%s;%s;%s;%s;%s;%s\n' % (today.date(), today.strftime('%H:%m:%S'), netcat_time, scp_time, speed, 4, 5, 6 , 7))
+        f = open('%s-%s-%s.csv' % (inst_type, region1, region2), 'a+')
+        f.write('%s;%s;%s;%s;%s;%s;%s;%s;%s\n' % (
+                today.date(), today.strftime('%H:%M:%S'), netcat_time, scp_time,
+                speed1, speed4, speed8, speed12, speed16))
         f.flush()
         f.close()
 
-        print 'ok'
-        time.sleep(1*60)
+        print inst_type, 'ok'
+        time.sleep(5)
 
     except:
 
-        print sys.exc_info()
+        print inst_type, sys.exc_info()
 
     finally:
 
-        print 'terminating instances ...'
-        conn1.terminate_instances(instance_ids=[inst1.id])
-        conn2.terminate_instances(instance_ids=[inst2.id])
+        #print inst_type, 'terminating instances ...'
+        #conn1.terminate_instances(instance_ids=[inst1.id])
+        #conn2.terminate_instances(instance_ids=[inst2.id])
+        pass
 
 
 def scp_test(ip_from, ip_to, filesize):
@@ -181,26 +177,26 @@ def scp_test(ip_from, ip_to, filesize):
     ssh_cli.connect(ip_from, username='ubuntu', key_filename=ssh_key)
 
     stdin, stdout, stderr = ssh_cli.exec_command('truncate -s %s /tmp/out.file' % filesize)
-    print stdout.read()
     print stderr.read()
 
     print 'scp test ...'
 
     print 'send %s to %s' % (filesize, ip_to)
+
     start_time = time.time()
-    stdin, stdout, stderr = ssh_cli.exec_command('scp /tmp/out.file ubuntu@%s:/tmp/scp.file' % ip_to)
+    stdin, stdout, stderr = ssh_cli.exec_command('scp /tmp/out.file ubuntu@%s:/dev/null' % ip_to)
     stdout.channel.recv_exit_status()
     end_time = time.time()
     print stderr.read()
     ssh_cli.close()
 
-    ssh_cli = paramiko.SSHClient()
-    ssh_cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_cli.connect(ip_to, username='ubuntu', key_filename=ssh_key)
-    stdin, stdout, stderr = ssh_cli.exec_command('ls -lah /tmp')
-    print stdout.read()
-    print stderr.read()
-    ssh_cli.close()
+    #ssh_cli = paramiko.SSHClient()
+    #ssh_cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    #ssh_cli.connect(ip_to, username='ubuntu', key_filename=ssh_key)
+    #stdin, stdout, stderr = ssh_cli.exec_command('ls -lah /tmp')
+    #print stdout.read()
+    #print stderr.read()
+    #ssh_cli.close()
 
     return end_time - start_time
 
@@ -213,7 +209,7 @@ def netcat_test(ip_from, ip_to, filesize):
             "-o", "UserKnownHostsFile=/dev/null",
             "-l", "ubuntu",
             ip_to,
-            'nc -l 7777 > /tmp/netcat.file'],
+            'nc -l 7777 > /dev/null'],
             stdout=subps.PIPE, stderr=subps.PIPE, close_fds=True)
     print 'run netcat listener on %s' % ip_to
 
@@ -224,29 +220,29 @@ def netcat_test(ip_from, ip_to, filesize):
     ssh_cli.connect(ip_from, username='ubuntu', key_filename=ssh_key)
 
     stdin, stdout, stderr = ssh_cli.exec_command('truncate -s %s /tmp/out.file' % filesize)
-    print stdout.read()
     print stderr.read()
 
     print 'netcat test ...'
 
-    start_time = time.time()
     stdin, stdout, stderr = ssh_cli.exec_command('sudo killall -9 nc')
     stdout.channel.recv_exit_status()
 
     print 'send %s to %s' % (filesize, ip_to)
+
+    start_time = time.time()
     stdin, stdout, stderr = ssh_cli.exec_command('cat /tmp/out.file | nc %s 7777 -q 0' % ip_to)
     stdout.channel.recv_exit_status()
     end_time = time.time()
     print stderr.read()
     ssh_cli.close()
 
-    ssh_cli = paramiko.SSHClient()
-    ssh_cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_cli.connect(ip_to, username='ubuntu', key_filename=ssh_key)
-    stdin, stdout, stderr = ssh_cli.exec_command('ls -lah /tmp')
-    print stdout.read()
-    print stderr.read()
-    ssh_cli.close()
+    #ssh_cli = paramiko.SSHClient()
+    #ssh_cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    #ssh_cli.connect(ip_to, username='ubuntu', key_filename=ssh_key)
+    #stdin, stdout, stderr = ssh_cli.exec_command('ls -lah /tmp')
+    #print stdout.read()
+    #print stderr.read()
+    #ssh_cli.close()
 
     return end_time - start_time
 
@@ -259,6 +255,7 @@ def iperf_test(ip_serv, ip_cli):
     stdin, stdout, stderr = ssh_serv.exec_command('sudo killall -9 iperf')
     stdout.channel.recv_exit_status()
     stdin, stdout, stderr = ssh_serv.exec_command('iperf -s -p 1234 &>/dev/null')
+
     print 'iperf server start'
 
     ssh_cli = paramiko.SSHClient()
@@ -266,7 +263,11 @@ def iperf_test(ip_serv, ip_cli):
     ssh_cli.connect(ip_cli, username='ubuntu', key_filename=ssh_key)
     stdin, stdout, stderr = ssh_cli.exec_command('sudo killall -9 iperf')
     stdout.channel.recv_exit_status()
-    stdin, stdout, stderr = ssh_cli.exec_command("iperf -c %s -p 1234 -t 5 | grep '/sec' >> /tmp/iperf.log" % ip_serv)
+
+    speed = []
+
+    stdin, stdout, stderr = ssh_cli.exec_command(
+            "iperf -c %s -p 1234 -t 20 -f m | grep '/sec' > /tmp/iperf.log" % ip_serv)
     stdout.channel.recv_exit_status()
     print stderr.read()
     stdin, stdout, stderr = ssh_cli.exec_command('cat /tmp/iperf.log')
@@ -274,10 +275,27 @@ def iperf_test(ip_serv, ip_cli):
     print stderr.read()
     out = stdout.read()
 
-    speed = 'unknown'
     for line in out.split('\n'):
         if line.endswith('/sec'):
-            speed = line.split()[-2]
+            print line
+            speed.append(line.split()[-2])
+
+
+    for p in [4, 8, 12, 16]:
+        stdin, stdout, stderr = ssh_cli.exec_command(
+                "iperf -c %s -p 1234 -t 20 -P %s -f m | grep '/sec' > /tmp/iperf.log" % (ip_serv, p))
+        stdout.channel.recv_exit_status()
+        print stderr.read()
+        stdin, stdout, stderr = ssh_cli.exec_command('cat /tmp/iperf.log')
+        stdout.channel.recv_exit_status()
+        print stderr.read()
+        out = stdout.read()
+
+        for line in out.split('\n'):
+            if '[SUM]' in line:
+                print line
+                speed.append(line.split()[-2])
+
 
     stdin, stdout, stderr = ssh_serv.exec_command('sudo killall -9 iperf')
     stdout.channel.recv_exit_status()
@@ -291,5 +309,22 @@ def iperf_test(ip_serv, ip_cli):
 
 if __name__ == '__main__':
 
-    network_io_test()
+    ps = []
 
+    #for itype in ['m1.small', 'm1.xlarge', 'c1.medium']:
+    for itype in ['m1.small']:
+
+        #ps.append(mp.Process(target=ec2_network_io_test,
+        #        kwargs={'inst_type':itype, 'filesize':'5M'}))
+        ps.append(mp.Process(target=ec2_network_io_test,
+                        kwargs={'inst_type':itype, 'region1':'us-east-1', 'ami1':'ami-3fec7956',
+                        'region2':'eu-west-1', 'ami2':'ami-f2191786', 'filesize':'5M'}))
+
+    for p in ps:
+        p.start()
+
+    for p in ps:
+        try:
+            p.join()
+        except KeyboardInterrupt:
+            pass
